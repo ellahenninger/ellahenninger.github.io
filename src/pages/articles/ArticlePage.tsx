b50a6a2ced4fdc2e0e1b8f8561a56a44f80dcedb
panteroni <panteroni@gmail.com>
2025-12-13 23:06:28 +0100

    Changing images path

--- src/pages/articles/ArticlePage.tsx
+++ src/pages/articles/ArticlePage.tsx
@@ -1,34 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { projects } from '../projectsData';
-import thumbAccra from '../../assets/Thumbnails/thumbnail_accra.jpeg';
-import thumbStrongDemand from '../../assets/Thumbnails/thumbnail_strong demand.png';
-import thumbBeyondHaze from '../../assets/Thumbnails/thumbnail_beyond the haze.jpeg';
-import thumbDelhi from '../../assets/Thumbnails/thumbnail_delhi.jpeg';
-import thumbShiftingPollution from '../../assets/Thumbnails/thumbnail_shifting pollution.jpeg';
-import thumbMapMunster from '../../assets/Thumbnails/thumbnail_map münster.jpeg';
-import thumbJakarta from '../../assets/Thumbnails/thumbnail_jakarta.jpeg';
-import thumbQualPaper from '../../assets/Thumbnails/thumbnail_qual paper.png';
-import thumbJoburg from '../../assets/Thumbnails/thumbnail_joburg.jpeg';
-import thumbPollutionHavens1 from '../../assets/Thumbnails/thumbnail_pollution havens1.jpeg';
-import thumbBikeMunster from '../../assets/Thumbnails/thumbnail_bike münster.jpeg';
-import thumbPollutionHavens2 from '../../assets/Thumbnails/thumbnail_pollution havens2.jpeg';
-import thumbPersistentInequalities from '../../assets/Thumbnails/thumbnail_persistent inequalities.jpeg';
 
-const thumbnails: Record<string, string> = {
-  'src/assets/Thumbnails/thumbnail_accra.jpeg': thumbAccra,
-  'src/assets/Thumbnails/thumbnail_strong demand.png': thumbStrongDemand,
-  'src/assets/Thumbnails/thumbnail_beyond the haze.jpeg': thumbBeyondHaze,
-  'src/assets/Thumbnails/thumbnail_delhi.jpeg': thumbDelhi,
-  'src/assets/Thumbnails/thumbnail_shifting pollution.jpeg': thumbShiftingPollution,
-  'src/assets/Thumbnails/thumbnail_map münster.jpeg': thumbMapMunster,
-  'src/assets/Thumbnails/thumbnail_jakarta.jpeg': thumbJakarta,
-  'src/assets/Thumbnails/thumbnail_qual paper.png': thumbQualPaper,
-  'src/assets/Thumbnails/thumbnail_joburg.jpeg': thumbJoburg,
-  'src/assets/Thumbnails/thumbnail_pollution havens1.jpeg': thumbPollutionHavens1,
-  'src/assets/Thumbnails/thumbnail_bike münster.jpeg': thumbBikeMunster,
-  'src/assets/Thumbnails/thumbnail_pollution havens2.jpeg': thumbPollutionHavens2,
-  'src/assets/Thumbnails/thumbnail_persistent inequalities.jpeg': thumbPersistentInequalities,
-};
 
 import './ArticlePage.css';
 import { useState, useCallback, useEffect } from 'react';
